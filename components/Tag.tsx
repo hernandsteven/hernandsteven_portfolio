ecda589871b4e9a0e06aa2ebe0b0cf14b825b740
hernandsteven <58962667+hernandsteven@users.noreply.github.com>
2023-02-14 16:43:21 -0500

    changed tag colors

--- components/Tag.tsx
+++ components/Tag.tsx
@@ -4,7 +4,7 @@ interface TagProps {
 
 export default function Tag({ tag }: TagProps) {
   return (
-    <div className="text-md w-fit rounded-md  border border-gray-800 bg-gradient-to-r  from-gray-800 to-transparent px-2 py-1 font-medium text-white shadow-sm shadow-sm shadow-gray-800">
+    <div className="md:text-md w-fit rounded-md border  border-gray-800 bg-gradient-to-r from-gray-800  to-transparent px-2 py-1 text-sm font-medium text-white shadow-sm shadow-sm shadow-gray-800">
       {tag}
     </div>
   )
